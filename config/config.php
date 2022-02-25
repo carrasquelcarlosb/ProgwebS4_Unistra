3da91f26a37d4b65513ed7126ce0a9ddea7c134d
CARRASQUEL-VILLARROEL CARLOS <carlos.carrasquel-villarroel@etu.unistra.fr>
2022-02-25 14:54:18 +0100

    Adjonction fichier configuration DATABASE

---
+++ config/config.php
@@ -0,0 +1,15 @@
+// DATABASE PARAMETERS
+define('DB_HOST', 'insert databse url here'); // Adjonction de HOST du DB
+define('DB_USER', 'admin'); // Adjonction de ROOT DB
+define('DB_PASS', ''); // Adjonction mot de passe ROOT
+define('DB_NAME', ''); // Adjonction du nom DB
+
+//APPROOT
+define('APPROOT', dirname(dirname(__FILE__)));
+
+//URLROOT(Dynamic links)
+define('URLROOT', 'https://');
+
+//Sitename
+define('SITENAME', 'Login & Register script');
+
