bcbc6870df43a66837807a8947e9f2bc0200b557
CARRASQUEL-VILLARROEL CARLOS <carlos.carrasquel-villarroel@etu.unistra.fr>
2022-03-08 11:37:53 +0100

    Adjonction de fichier config.php pour la base des donnees

--- config/config.php
+++ config/config.php
@@ -1,15 +1,15 @@
-
-// DATABASE PARAMETERS
+<?php
+//DATABASE PARAMETERS
 define('DB_HOST', 'carrasquel'); // Adjonction de HOST du DB
 define('DB_USER', 'admin'); // Adjonction de ROOT DB
 define('DB_PASS', 'daswaG5H'); // Adjonction mot de passe ROOT
 define('DB_NAME', 'carrasquel'); // Adjonction du nom DB
 
-//APPROOT
+APPROOT
 define('APPROOT', dirname(dirname(__FILE__)));
 
 //URLROOT(Dynamic links)
-define('URLROOT', 'https://osr-etudiant.unistra.fr/~%3Ccarrasquel%3E/');
+define('URLROOT', 'https://osr-etudiant.unistra.fr/~carrasquel/');
 
 //Sitename
 define('SITENAME', 'Login & Register script');
