b394a6cfc9c79c3fb42c9e1279a8f71d9c6972dc
CARRASQUEL-VILLARROEL CARLOS <carlos.carrasquel-villarroel@etu.unistra.fr>
2022-03-15 16:40:00 +0100

    Modification de fichier config sur le macro URLROOT pour prendre le chemin correcte

--- config/config.php
+++ config/config.php
@@ -9,7 +9,7 @@ define('DB_PASS', 'daswaG5H'); // Adjonction mot de passe ROOT
 define('APPROOT', dirname(dirname(__FILE__)));
 
 //URLROOT(Dynamic links)
-define('URLROOT', 'https://osr-etudiant.unistra.fr/~carrasquel/');
+define('URLROOT', 'https://osr-etudiant.unistra.fr/~carrasquel/progwebs4');
 
 //Sitename
 define('SITENAME', 'Login & Register script');
