e0606b2b10a44dba42962135b266bf2186f23d26
CARRASQUEL-VILLARROEL CARLOS <carlos.carrasquel-villarroel@etu.unistra.fr>
2022-03-15 17:44:16 +0100

    debuggin view/articles/index #5

--- view/articles/index.php
+++ view/articles/index.php
@@ -10,29 +10,29 @@
 </div>
 
 <div class= "container">
-  <?php if( true == isLoggedIn())?>
+  <?php if( true == isLoggedIn())
+endif;?>
     <a class="button green" href = "<?php echo URLROOT;?>/articles/create">
       New Article
     </a>
 
-  <?php foreach($data['articles'] as $article): ?>
+  <?php foreach($data['articles'] as $article): endforeach;?>
     <div class='container-item'>
-    <php if(isset($_SESSION['user_id']) && $_SESSION['user_id'] == $post->user_id): ?>
-      <a class="button transparent"
-        href = "<?php echo URLROOT;?> ./articles/update.php". $article->id >
-        Update
-      </a>
-
-    <h2>
-      <?php $article->$title; ?>
-    </h2>
-
-    <h3>
-      <? php echo 'Created on: ' . date('F j h:m' strtotime($articles->createdAt))?>
-    </h3>
-
-    <p>
-        <? php echo $article->body?>
-    </p>
-  </div>
+        <?php if(isset($_SESSION['user_id']) && $_SESSION['user_id'] == $post->user_id):
+            endif; ?>
+            <a class="button transparent"
+                href = "<?php echo URLROOT;?> ./articles/update.php". $article->id >
+                Update
+            </a>
+            <h2>
+                <?php $article->$title; ?>
+            </h2>
+            <h3>
+                <? php echo 'Created on: '
+                 . date('F j h:m' strtotime($articles->createdAt))?>
+            </h3>
+            <p>
+                <? php echo $article->body?>
+            </p>
+    </div>
 </div>
