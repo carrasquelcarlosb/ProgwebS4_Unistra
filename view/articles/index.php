9fa59421d3e3fb1c2656afc817707c93f300318c
CARRASQUEL-VILLARROEL CARLOS <carlos.carrasquel-villarroel@etu.unistra.fr>
2022-03-15 17:24:29 +0100

    debuggin view/articles/index #2

--- view/articles/index.php
+++ view/articles/index.php
@@ -19,10 +19,10 @@
     <div class='container-item'>
     <php if(isset($_SESSION['user_id']) && $_SESSION['user_id'] == $post->user_id): ?>
       <a class="button transparent"
-      href = <?php echo URLROOT "./articles/update". $article->id ?> >
+        href = "<?php echo URLROOT "./articles/update.php". $article->id ?>"
         Update
       </a>
-    
+
     <h2>
       <?php $article->$title; ?>
     </h2>
