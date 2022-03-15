c9d94c412d5e181cfbf3974f45eb9015d5fd51dc
CARRASQUEL-VILLARROEL CARLOS <carlos.carrasquel-villarroel@etu.unistra.fr>
2022-03-15 16:38:35 +0100

    Modification des liens sur navigation.php

--- view/includes/navigation.php
+++ view/includes/navigation.php
@@ -1,19 +1,13 @@
 <nav class ="top-nav">
     <ul>
         <li>
-            <a href="<?php echo URLROOT;?>/pages/index"> Home </a>
+            <a href="<?php echo URLROOT;?>index.php"> Home </a>
         </li>
         <li>
-            <a href="<?php echo URLROOT;?>/pages/index"> About </a>
-        </li>
-        <li>
-            <a href="<?php echo URLROOT;?>/pages/index"> Articles </a>
-        </li>
-        <li>
-            <a href="<?php echo URLROOT;?>/pages/index"> Contact Us </a>
+            <a href="<?php echo URLROOT;?>view/articles/index.php"> Articles </a>
         </li>
         <li class ="btn-login">
-            <a href="<?php echo URLROOT;?>/pages/index"> Login </a>
+            <a href="<?php echo URLROOT;?>users/index.php"> Login </a>
         </li>
     </ul>
 </nav>
