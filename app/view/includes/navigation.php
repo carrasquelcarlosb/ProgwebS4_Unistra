8c036e1cd74e8dc4c65340ffdf0256f30a15e8b8
CARRASQUEL-VILLARROEL CARLOS <carlos.carrasquel-villarroel@etu.unistra.fr>
2022-04-18 02:30:03 +0200

    Chainage des fichiers

--- app/view/includes/navigation.php
+++ app/view/includes/navigation.php
@@ -4,10 +4,10 @@
             <a href="index.php"> Home </a>
         </li>
         <li>
-            <a href="carrasquel/app/view/articles/index.php"> Articles </a>
+            <a href="app/view/articles/index.php"> Articles </a>
         </li>
         <li class ="btn-login">
-            <a href="carrasquel/app/view/admin/login.php"> Login </a>
+            <a href="app/view/admin/login.php"> Login </a>
         </li>
     </ul>
 </nav>
