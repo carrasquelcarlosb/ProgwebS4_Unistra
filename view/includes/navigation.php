24226bb03b5a909ccb81d4996a93477f2ea41bcd
CARRASQUEL-VILLARROEL CARLOS <carlos.carrasquel-villarroel@etu.unistra.fr>
2022-04-18 02:54:19 +0200

    index modif

--- view/includes/navigation.php
+++ view/includes/navigation.php
@@ -4,10 +4,10 @@
             <a href="index.php"> Home </a>
         </li>
         <li>
-            <a href="app/view/articles/index.php"> Articles </a>
+            <a href="view/articles/index.php"> Articles </a>
         </li>
         <li class ="btn-login">
-            <a href="app/view/admin/login.php"> Login </a>
+            <a href="view/admin/login.php"> Login </a>
         </li>
     </ul>
 </nav>
